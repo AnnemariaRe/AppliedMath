b14b68edece798557da25dead2ebd2c16779c988
Annemaria <annemariarepenko@gmail.com>
2022-03-17 21:21:19 +0300

    fix: null

--- brent.py
+++ brent.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from math import sin, sqrt
 
 ratio = (sqrt(5) - 1) / 2
@@ -8,7 +9,7 @@ def f(x):
 
 def parabolic_approximation(x1, x2, x3, f1, f2, f3):
     if (2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1))) == 0:
-        return 0
+        return NULL
     else:
         u = x2 - ((x2 - x1) ** 2 * (f2 - f3) - (x2 - x3) ** 2 * (f2 - f1)) / (
                 2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1)))
@@ -25,7 +26,7 @@ def brent_min(a, b, e):
         g = dp / 2
         dp = dc
         u = parabolic_approximation(x, w, v, fx, fw, fv)
-        if (u == 0) or ((a <= u <= b) != 1) or (abs(u - x) > g):
+        if (u == NULL) or ((a <= u <= b) != 1) or (abs(u - x) > g):
             if x < (a + b) / 2:
                 u = x + ratio * (b - x)
                 dp = b - x
