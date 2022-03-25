0d7f8e4b7833bab73f8f7d258760b895b601f789
Kerenery <djhitekdieant@gmail.com>
2022-03-25 08:10:05 +0300

    fix: golden ratio oracle calls decrease

--- goldenratio.py
+++ goldenratio.py
@@ -8,25 +8,37 @@ def f(x):
 
 
 def search_min(a, b, eps):
-    assert a < b, 'Incorrect input of interval (ಠ ͜ʖಠ)'
-    iterations = 0
+    assert a < b, 'Incorrect input of interval'
+
     segments = []
+
+    iterations = 0
     oracle_calls = 0
+
+    x1 = a + ratio * (b - a)
+    x2 = b - ratio * (b - a)
+
+    f1 = f(x1)
+    f2 = f(x2)
+
+    oracle_calls += 2
+
     while b - a > eps:
-        d = ratio * (b - a)
-        x1 = a + d
-        x2 = b - d
-        fx1 = f(x1)
-        fx2 = f(x2)
-        oracle_calls += 2
-        if fx1 > fx2:
-            b = x1
-        elif fx1 < fx2:
-            a = x2
+        if f1 > f2:
+            a = x1
+            x1 = x2
+            f1 = f2
+            x2 = b - ratio * (b - a)
+            f2 = f(x2)
         else:
-            a, b = x1, x2
+            b = x2
+            x2 = x1
+            f2 = f1
+            x1 = a + ratio * (b - a)
+            f1 = f(x1)
 
         iterations += 1
+        oracle_calls += 1
         segments.append((b - a))
 
     return [((a + b) / 2, iterations), oracle_calls, segments]
